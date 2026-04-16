ac2e395dad042e08b9ffdaf3c7cfbae05f83b9d5
varunvora05 <varunvora20@gmail.com>
2026-04-16 03:31:33 -0400

    Added C# class library

---
+++ StringExtensionsLib/StringExtensionsLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
